df46e278e597411ac4b065e2ba76e3537e55ae7f
DanielAkmal <daniel.titanstech@gmail.com>
2022-12-12 15:35:32 +0700

    Add Vita Character, and Texture Swap Function

---
+++ Assets/Scripts/SwapController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapController : MonoBehaviour
+{
+    public Animator anim;
+    public SwapTexture swapTexture;
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10,10,200,50), "SwitchPose"))
+        {
+            anim.SetTrigger("SwitchPose");
+            swapTexture.Invoke("SwitchTexture", 0.3f);
+        }
+    }
+
+    public void AnimationEventSwapTexture()
+    {
+        anim.SetTrigger("SwitchPose");
+        swapTexture.Invoke("SwitchTexture", 0.3f);
+    }
+}
---
+++ Assets/Scripts/SwapTexture.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct TextureSwapper
+{
+    public string name;
+    public Material mat;
+    public Texture texture_1;
+    public Texture texture_2;
+}
+
+public class SwapTexture : MonoBehaviour
+{
+    public List<TextureSwapper> textureSwapper;
+
+    public int textureIndex = 1;
+
+    public void SwitchTexture()
+    {
+        if (textureIndex == 1)
+        {
+            foreach (var item_swap in textureSwapper)
+            {
+                item_swap.mat.SetTexture("_MainTex", item_swap.texture_2);
+                item_swap.mat.SetTexture("_ShadeTexture", item_swap.texture_2);
+            }
+
+            textureIndex = 2;
+        }
+        else
+        {
+            foreach (var item_swap in textureSwapper)
+            {
+                item_swap.mat.SetTexture("_MainTex", item_swap.texture_1);
+                item_swap.mat.SetTexture("_ShadeTexture", item_swap.texture_1);
+            }
+
+            textureIndex = 1;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (textureIndex == 2)
+        {
+            SwitchTexture();
+        }
+    }
+}
