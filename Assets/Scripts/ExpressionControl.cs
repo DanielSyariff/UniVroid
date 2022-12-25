73bb1e35fcbc65af7156cafce90ea88b1ed11c92
DanielAkmal <daniel.titanstech@gmail.com>
2022-12-25 22:36:53 +0700

    Add Expression Animation

---
+++ Assets/Scripts/ExpressionControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExpressionControl : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private Slider sliderBlend;
+    
+    public void ChangeExpression(string trigger)
+    {
+        anim.SetTrigger(trigger);
+    }
+
+    private void Update()
+    {
+        anim.SetFloat("Blend", sliderBlend.value);
+    }
+}
